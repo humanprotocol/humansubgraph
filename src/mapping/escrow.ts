f943b3b2b03c6b6c268cf87cefb63eaff7e7792d
posix4e <posix4e@gmail.com>
2022-05-14 22:36:05 -0400

    Add CI and deploy script

--- src/mapping/escrow.ts
+++ src/mapping/escrow.ts
@@ -4,7 +4,7 @@ import {
   IntermediateStorage,
   Pending
 } from "../generated/templates/Escrow/Escrow"
-import { Escrow } from "../generated/schema"
+import { Escrow} from "../generated/schema"
 
 export function handleIntermediateStorage(event: IntermediateStorage): void {
   // Entities can be loaded from the store using a string ID; this ID
@@ -29,41 +29,6 @@ export function handleIntermediateStorage(event: IntermediateStorage): void {
 
   // Entities can be written to the store with `.save()`
   entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.areTrustedHandlers(...)
-  // - contract.bulkPaid(...)
-  // - contract.bulkPayOut(...)
-  // - contract.cancel(...)
-  // - contract.canceler(...)
-  // - contract.duration(...)
-  // - contract.eip20(...)
-  // - contract.finalAmounts(...)
-  // - contract.finalResultsHash(...)
-  // - contract.finalResultsUrl(...)
-  // - contract.getBalance(...)
-  // - contract.launcher(...)
-  // - contract.manifestHash(...)
-  // - contract.manifestUrl(...)
-  // - contract.recordingOracle(...)
-  // - contract.recordingOracleStake(...)
-  // - contract.reputationOracle(...)
-  // - contract.reputationOracleStake(...)
-  // - contract.status(...)
 }
 
 export function handlePending(event: Pending): void {
@@ -90,39 +55,4 @@ export function handlePending(event: Pending): void {
   // Entities can be written to the store with `.save()`
   entity.save()
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.areTrustedHandlers(...)
-  // - contract.bulkPaid(...)
-  // - contract.bulkPayOut(...)
-  // - contract.cancel(...)
-  // - contract.canceler(...)
-  // - contract.duration(...)
-  // - contract.eip20(...)
-  // - contract.finalAmounts(...)
-  // - contract.finalResultsHash(...)
-  // - contract.finalResultsUrl(...)
-  // - contract.getBalance(...)
-  // - contract.launcher(...)
-  // - contract.manifestHash(...)
-  // - contract.manifestUrl(...)
-  // - contract.recordingOracle(...)
-  // - contract.recordingOracleStake(...)
-  // - contract.reputationOracle(...)
-  // - contract.reputationOracleStake(...)
-  // - contract.status(...)
 }
-
